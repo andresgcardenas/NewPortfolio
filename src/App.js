1e38eefed0f6e4e2f7d33b3cad7311b6a666289f
unknown <andrescardenas34@outlook.com>
2019-04-23 23:55:37 -0400

    Front page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-
+import Main from './components/main';
 
 function App() {
   return (
@@ -23,6 +23,7 @@ function App() {
                 <a href="/">Link</a>
             </Navigation>
         </Drawer>
+        <Main/>
         <Content />
     </Layout>
 </div>
