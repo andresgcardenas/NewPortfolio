c0cb5cc0f73e9158d6d9c44cc95a38f66754f08a
unknown <andrescardenas34@outlook.com>
2019-04-24 23:01:10 -0400

    more changes, wanting this to come out very neat and professional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 
 
 function App() {
   return (
       
-<div style={{height: '1000px', position: 'relative'}} className='header-color'>
+<div style={{height: '2000px', width:'100%'}} className='header-color1'>
 <Navbar bg="light" expand="lg" className='header-color'>
-  <Navbar.Brand href="#home">Porfoilio!</Navbar.Brand>
+  <Navbar.Brand href="#home">Welcome!</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="#link">About Me</Nav.Link>
-      <Nav.Link href="/">Portfolio</Nav.Link>
-      <Nav.Link href="#link">Resume</Nav.Link>
+      <Nav.Link href="/aboutme">About Me</Nav.Link>
+      <Nav.Link href="/projects">Projects</Nav.Link>
+      <Nav.Link href="/resume">Resume</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>;
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,54 +1,36 @@
 import React, {Component} from 'react';
-import {Grid, Cell} from 'react-mdl';
-import myself from '../img/myself.jpg'
+import myself from '../img/myself.jpg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 class Landing extends Component {
     render(){
         return(
-            <div style={{width: '100%', margin: 'auto'}}>
-                <Grid className="landing-grid">
-                    <Cell col={12}>
+            <div>
+                <Container className="landing-Container">
+                    <Row>
+                    <Col sm={12}>
                     <p className="gap"/>
                     <img
                         src={myself}
                         alt='myself'
                         className='myself-img'/> 
                         <p/>
-                    <div>
-                        <h1 className="landing-intro">Hello! My names Andres Cardenas.</h1>
-                        <h4 className="landing-intro" >
-                        Ive grown a passion for web developement,
-                        i continue to expand my knowledge and become
-                        the best programmer i can be.
-                        
-                        </h4>
-                    </div>        
-                    <p className="gap"/>
-                    <div className='banner-text'>
-                        <h1>Full Stack Web Devloper</h1>
-                        <hr/>
-
-
-                        <p>JavaScript | React | NodeJS | MongoDB | Express.js | Html/Css | PHP</p>
-
-                        <div className="social-links">
-                            {/* LinkedIn */}
-                            <a href="https://www.linkedin.com/in/andres-cardenas-03860a158" rel="noopener noreferrer" target="_blank">
-                                <i className="fab fa-linkedin" aria-hidden="true"/>
-                            </a>
-                            {/* Github */}
-                            <a href="https://github.com/Yungcode34" rel="noopener noreferrer" target="_blank">
-                                <i className="fab fa-github-square" aria-hidden="true"/>
-                            </a>
-                            {/* Instagram */}
-                            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                <i className="fab fa-instagram" aria-hidden="true"/>
-                            </a>
-
-                    </div>
-                    </div>
-                    </Cell>
-                </Grid>
+                    </Col>
+                    </Row>
+                    <Row>
+                    <Col sm={6}>
+                        <h1>Hello! My name is Andres Cardenas</h1>
+                    </Col>
+                    <Col sm={6}>
+                    <p className="description-bio">im a 20 year old full stack web developer</p>
+                    <p className="description-bio">specializing in javascript with various skills and</p>
+                    <p className="description-bio">expirience with different technologies</p>
+                    </Col>
+                    </Row>
+                </Container>
             </div>
+        
         )
     }
 }
