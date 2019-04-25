21cb8dba3eec98b277960b1f1c2ec089d1deb111
unknown <andrescardenas34@outlook.com>
2019-04-25 00:16:32 -0400

    more changes...

--- src/App.js
+++ src/App.js
@@ -9,16 +9,17 @@ import Navbar from 'react-bootstrap/Navbar'
 function App() {
   return (
       
-<div style={{height: '2000px', width:'100%'}} className='header-color1'>
-<Navbar bg="light" expand="lg" className='header-color'>
-  <Navbar.Brand href="#home">Welcome!</Navbar.Brand>
+<div style={{height: '1750px', width:'100%'}} className='header-color1'>
+<Navbar fixed="top" bg="light" expand="lg" className='header-color'>
+  <Navbar.Brand href="/"><h3 className="nav-text">Welcome!</h3></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/aboutme">About Me</Nav.Link>
-      <Nav.Link href="/projects">Projects</Nav.Link>
-      <Nav.Link href="/resume">Resume</Nav.Link>
+      <Nav.Link href="/"><h6 className="nav-text">Home</h6></Nav.Link>
+      <Nav.Link href="/aboutme"><h6 className="nav-text">About Me</h6></Nav.Link>
+      <Nav.Link href="/projects"><h6 className="nav-text">Resume</h6></Nav.Link>
+      <Nav.Link href="/resume"><h6 className="nav-text">Projects</h6></Nav.Link>
+      <Nav.Link href="/contact"><h6 className="nav-text">Contact Me</h6></Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>;
