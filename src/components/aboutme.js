24901a7318e15c44e98a9447634fae0d3af88bb9
unknown <andrescardenas34@outlook.com>
2019-04-26 22:20:29 -0400

    more updates

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -28,14 +28,73 @@ class Resume extends Component {
                     <Row>
                     <Col sm={6}>
                         <Fade left>
-                        <h1 className="intro">What do i do?</h1>
+                        <h1 className="intro">A little about me</h1>
                         </Fade>
                     </Col>
                     <Col sm={6}>
                     <Fade right>
-                    <p className="description-bio1">Im a highly encouraged individuals who loves to program</p>
+                    <p className="description-bio1">Im a highly encouraged individual who loves to program</p>
                     <p className="description-bio">I want to be able to contribute to the world with the power of technology</p>
                     <p className="description-bio">Feel free to look around and find out more about me!</p>
+                    </Fade>
+                    </Col>
+                    </Row>
+                    <Row>
+                    <Col sm={12}>
+                        <Fade bottom>
+                        <h1 className="introskills">Skills</h1>
+                        </Fade>
+                    </Col>
+                    </Row>
+                    <Row className="icons">
+                    <Col sm={3}>
+                        <Fade bottom>
+                        <img src="https://img.icons8.com/color/96/000000/javascript.png"/>
+                        <p className="description-bio">JavaScript</p>
+                        </Fade>
+                    </Col>
+                    <Col sm={3}>
+                    <Fade bottom>
+                    <img src="https://img.icons8.com/ios/100/000000/react-native.png" style={{height:'96px'}}/>
+                    <p className="description-bio">React</p>
+                    </Fade>
+                    </Col>
+                    <Col sm={3}>
+                        <Fade bottom>
+                        <img src="https://img.icons8.com/color/96/000000/nodejs.png"/>
+                        <p className="description-bio">Node.js</p>
+                        </Fade>
+                    </Col>
+                    <Col sm={3}>
+                    <Fade bottom>
+                    <img src="https://img.icons8.com/color/96/000000/mongodb.png"/>
+                    <p className="description-bio">Mongo DB</p>
+                    </Fade>
+                    </Col>
+                    </Row>
+                    <Row className="icons">
+                    <Col sm={3}>
+                        <Fade bottom>
+                        <img src="https://img.icons8.com/color/96/000000/html-5.png"/>
+                        <p className="description-bio">HTML 5</p>
+                        </Fade>
+                    </Col>
+                    <Col sm={3}>
+                    <Fade bottom>
+                    <img src="https://img.icons8.com/color/96/000000/css3.png"/>
+                    <p className="description-bio">CSS / css libraries</p>
+                    </Fade>
+                    </Col>
+                    <Col sm={3}>
+                    <Fade bottom>
+                    <img src="https://img.icons8.com/color/96/000000/api-settings.png"/>
+                    <p className="description-bio">REST / API development</p>
+                    </Fade>
+                    </Col>
+                    <Col sm={3}>
+                    <Fade bottom>
+                    <img src="https://img.icons8.com/color/96/000000/api-settings.png"/>
+                    <p className="description-bio">JavaScript</p>
                     </Fade>
                     </Col>
                     </Row>
