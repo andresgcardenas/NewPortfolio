d26afcebb5a88765d0353b92bb4bc392702bab49
butterrzz <38452206+butterrzz@users.noreply.github.com>
2019-04-28 21:48:44 -0400

    trying again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const nodeMailer = require('nodemailer');
-const path = required('path');
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
